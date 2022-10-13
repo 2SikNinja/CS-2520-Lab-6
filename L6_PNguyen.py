f4f24555642dc80bc4aa35be567e29b52743b08c
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-13 14:41:44 -0700

    third commit

--- L6_PNguyen.py
+++ L6_PNguyen.py
@@ -19,21 +19,27 @@
 #Task 2
 
 
-#Task 3
-names = ("Jack","Ada","Lev","Kay","Winny")
-ages = [20,18,22,16,20]
-#seperate names and ages for the additional case
-#names = ("Jeffery","Demarcus","Jason","Kim","David","Pat","Jielson","Manson")
-#ages = [22,17,18,21,25,16,17,19]
-nameAgePair = tuple(zip(names,ages))
-print(nameAgePair)
-youngestIndex = ages.index(min(ages))
-youngestPerson = names[youngestIndex]
-print(youngestPerson, "is the youngest at age",min(ages))
-average = sum(ages)/len(ages)
-print("Average age is:",average)
-newNames = [i for i in names if ages[names.index(i)]>average]
-newAges = [i for i in ages if i>average]
-newNameAgePair = tuple(zip(newNames,newAges))
-print(newNameAgePair)
 
+
+
+
+
+
+# #Task 3
+# names = ("Jack","Ada","Lev","Kay","Winny")
+# ages = [20,18,22,16,20]
+# #seperate names and ages for the additional case
+# #names = ("Jeffery","Demarcus","Jason","Kim","David","Pat","Jielson","Manson")
+# #ages = [22,17,18,21,25,16,17,19]
+# nameAgePair = tuple(zip(names,ages))
+# enumerate(nameAgePair)
+# print(nameAgePair)
+# youngestIndex = ages.index(min(ages))
+# youngestPerson = names[youngestIndex]
+# print(youngestPerson, "is the youngest at age",min(ages))
+# average = sum(ages)/len(ages)
+# print("Average age is:",average)
+# newNames = [i for i in names if ages[names.index(i)]>average]
+# newAges = [i for i in ages if i>average]
+# newNameAgePair = tuple(zip(newNames,newAges))
+# print(newNameAgePair)
