18d27144a7a0ce26a49603003301408a15782391
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-13 04:42:24 -0700

    second commit

--- L6_PNguyen.py
+++ L6_PNguyen.py
@@ -20,29 +20,20 @@
 
 
 #Task 3
+names = ("Jack","Ada","Lev","Kay","Winny")
+ages = [20,18,22,16,20]
+#seperate names and ages for the additional case
+#names = ("Jeffery","Demarcus","Jason","Kim","David","Pat","Jielson","Manson")
+#ages = [22,17,18,21,25,16,17,19]
+nameAgePair = tuple(zip(names,ages))
+print(nameAgePair)
+youngestIndex = ages.index(min(ages))
+youngestPerson = names[youngestIndex]
+print(youngestPerson, "is the youngest at age",min(ages))
+average = sum(ages)/len(ages)
+print("Average age is:",average)
+newNames = [i for i in names if ages[names.index(i)]>average]
+newAges = [i for i in ages if i>average]
+newNameAgePair = tuple(zip(newNames,newAges))
+print(newNameAgePair)
 
-j=0
-names = ("Jack", "Ada", "Lev", "Kay", "Winny")
-ages = [20, 18, 22, 16, 20]
-n = len(names)
-tupleList = names[0],names[1],names[2],names[3],names[4]
-tupleLists = ((n,n+1),)*n
-tupleLists = tupleList[0], tupleList[1], tupleList[2], tupleList[3], tupleList[4], 
-print(tupleLists)
-print(len(tupleLists[0][1]))
-print(tupleList)
-tup1 = (123, "abc", 4.5, ("in", 2), "end")
-print(tup1)
-tup2 = tuple ("bar")
-atp = (1, 2, 3)
-tup1 = atp[0], atp[1], atp[-1]
-tup3 = tup1 + tup2
-print(tup3)
-
-
-
-# for i in names:
-#     for k in names:
-#         tupleList
-#     j+=1
-# print(tupleList)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+j=0
+# names = ("Jack", "Ada", "Lev", "Kay", "Winny")
+# ages = [20, 18, 22, 16, 20]
+# n = len(names)
+# tupleList = names[0],names[1],names[2],names[3],names[4]
+# tupleLists = ((n,n+1),)*n
+# tupleLists = tupleList[0], tupleList[1], tupleList[2], tupleList[3], tupleList[4], 
+# print(tupleLists)
+# print(len(tupleLists[0][1]))
+# print(tupleList)
